627260eb89eb859dc2fdc817ad7a4fdd98a6cd79
YaHu-Lee <lyh990719@gmail.com>
2021-05-05 19:49:17 +0800

    init:rollup & svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script>
-	export let name;
+	let todos = [{
+		content: '吃早饭',
+		done: false,
+		id: 1
+	}, {
+		content: '吃午饭',
+		done: false,
+		id: 2
+	}]
+	function handleTodoClick({ id }) {
+		todos.forEach(todo => {
+			if(todo.id === id) todo.done = !todo.done
+		})
+		todos = todos
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="main">
+	{#each todos as todo(todo.id)}
+		<div class={todo.done ? "done" : "undo"} on:click={() => handleTodoClick(todo)}>{todo.content}</div>
+	{/each}
+	
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.done {
+		color: gray
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.undo {
+		color: black
 	}
 </style>
